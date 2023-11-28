4a1f3f01bbaa0e1b61ad653c1311e96c78f0aa6c
dr03ramos <dr03ramos@gmail.com>
2023-11-27 23:28:59 -0300

    New methods in TreinadorRepository class

--- src/br/edu/ifsul/pokemao/persistencia/TreinadorRepository.java
+++ src/br/edu/ifsul/pokemao/persistencia/TreinadorRepository.java
@@ -44,4 +44,142 @@ public class TreinadorRepository {
         }
         return len;
     }
+
+    public Treinador buscarPorUser(String user) {
+        Treinador treinador = null;
+        try {
+            this.conexao.abrirConexao();
+            String sqlInsert = "SELECT * FROM treinador WHERE user = ?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sqlInsert);
+            statement.setString(1, user);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                treinador = new Treinador(
+                    rs.getLong("id"),
+                    rs.getString("user"),
+                    rs.getString("senha"),
+                    rs.getString("nome"),
+                    rs.getInt("idade")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.conexao.fecharConexao();
+        }
+        return treinador;
+    }
+
+    public Treinador buscarPorID(long id) {
+        Treinador treinador = null;
+        try {
+            this.conexao.abrirConexao();
+            String sqlInsert = "SELECT * FROM treinador WHERE id = ?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sqlInsert);
+            statement.setLong(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                treinador = new Treinador(
+                    rs.getLong("id"),
+                    rs.getString("user"),
+                    rs.getString("senha"),
+                    rs.getString("nome"),
+                    rs.getInt("idade")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.conexao.fecharConexao();
+        }
+        return treinador;
+    }
+
+    public boolean login(String user, String senha) {
+        Treinador treinador = buscarPorUser(user);
+        if (treinador != null) {
+            if (treinador.getSenha().equals(senha)) {
+                this.treinadorLogado = treinador;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public long cadastrar(Treinador treinador) {
+        long id = 0;
+        try {
+            this.conexao.abrirConexao();
+            String sqlInsert = "INSERT INTO treinador(user, senha, nome, idade) VALUES(?, ?, ?, ?)";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sqlInsert);
+            statement.setString(1, treinador.getUser());
+            statement.setString(2, treinador.getSenha());
+            statement.setString(3, treinador.getNome());
+            statement.setInt(4, treinador.getIdade());
+            int linhas = statement.executeUpdate();
+            if (linhas > 0) {
+                ResultSet rs = statement.getGeneratedKeys();
+                if (rs.next()) {
+                    id = rs.getLong(1);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.conexao.fecharConexao();
+        }
+        return id;
+    }
+
+    public void logout() {
+        this.treinadorLogado = null;
+    }
+
+    public boolean atualizar(Treinador treinador) {
+        boolean retorno = false;
+        try {
+            this.conexao.abrirConexao();
+            String sqlInsert = "UPDATE treinador SET user=?, senha=?, nome=?, idade=? WHERE id=?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sqlInsert);
+            statement.setString(1, treinador.getUser());
+            statement.setString(2, treinador.getSenha());
+            statement.setString(3, treinador.getNome());
+            statement.setInt(4, treinador.getIdade());
+            statement.setLong(5, treinador.getId());
+            int linhasAfetadas = statement.executeUpdate();
+            if (linhasAfetadas > 0) {
+                retorno = true;
+            } else {
+                retorno = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            retorno = false;
+        } finally {
+            this.conexao.fecharConexao();
+        }
+        return retorno;
+    }
+
+    public boolean remover(long id) {
+        boolean retorno = false;
+        try {
+            this.conexao.abrirConexao();
+            String sqlInsert = "DELETE FROM treinador WHERE id=?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sqlInsert);
+            statement.setLong(1, id);
+            int linhasAfetadas = statement.executeUpdate();
+            if (linhasAfetadas > 0) {
+                retorno = true;
+            } else {
+                retorno = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            retorno = false;
+        } finally {
+            this.conexao.fecharConexao();
+        }
+        return retorno;
+    }
 }
